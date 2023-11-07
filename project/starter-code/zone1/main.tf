676fe9d40a423b2006679cfc0d5cb197c5f85d4f
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-11-07 11:47:42 +0100

    Update main.tf

--- project/starter-code/zone1/main.tf
+++ project/starter-code/zone1/main.tf
@@ -1,30 +1,40 @@
 locals {
-   account_id = data.aws_caller_identity.current.account_id
+  account_id = data.aws_caller_identity.current.account_id
 
-   name   = "udacity"
-   region = "us-east-2"
-   tags = {
-     Name      = local.name
-     Terraform = "true"
-   }
- }
- provider "aws" {
-     region = "us-west-1"
- }
- module "vpc" {
-   source     = "./modules/vpc"
-   cidr_block = "10.100.0.0/16"
+  name   = "udacity"
+  region = "us-east-2"
+  tags = {
+    Name      = local.name
+    Terraform = "true"
+  }
+}
 
-   account_owner = local.name
-   name          = "${local.name}-project"
-   azs           = ["us-east-2a","us-east-2b"]
-   private_subnet_tags = {
-     "kubernetes.io/role/internal-elb" = 1
-   }
-   public_subnet_tags = {
-     "kubernetes.io/role/elb" = 1
-   }
- }
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = "10.100.0.0/16"
+
+  account_owner = local.name
+  name          = "${local.name}-project"
+  azs           = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+output "vpc_id_east" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnet_ids_east" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "public_subnet_ids_east" {
+  value = module.vpc.public_subnet_ids
+}
 
 module "vpc_west" {
   source     = "./modules/vpc"
@@ -40,18 +50,18 @@ module "vpc_west" {
     "kubernetes.io/role/elb" = 1
   }
   providers = {
-    aws = aws.usw1  # Use the AWS provider defined at the root level
+    aws = aws.usw1
   }
-
 }
+
 output "vpc_id" {
-   value = module.vpc_west.vpc_id
- }
+  value = module.vpc_west.vpc_id
+}
 
- output "private_subnet_ids" {
-   value = module.vpc_west.private_subnet_ids
- }
+output "private_subnet_ids" {
+  value = module.vpc_west.private_subnet_ids
+}
 
- output "public_subnet_ids" {
-   value = module.vpc_west.public_subnet_ids
- }
+output "public_subnet_ids" {
+  value = module.vpc_west.public_subnet_ids
+}
