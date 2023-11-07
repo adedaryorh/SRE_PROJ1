2c398cda4198d6981df4ef4ef10ee84615cbec16
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-11-07 10:33:43 +0100

    Update main.tf

--- project/starter-code/zone1/main.tf
+++ project/starter-code/zone1/main.tf
@@ -24,21 +24,21 @@ locals {
    }
  }
 
-  module "vpc_west" {
+module "vpc_west" {
    source     = "./modules/vpc"
    cidr_block = "10.100.0.0/16"
-
+   
    account_owner = local.name
    name          = "${local.name}-project"
    azs           = ["us-west-1b","us-west-1c"]
    private_subnet_tags = {
-     "kubernetes.io/role/internal-elb" = 1
+   "kubernetes.io/role/internal-elb" = 1
    }
    public_subnet_tags = {
-     "kubernetes.io/role/elb" = 1
+   "kubernetes.io/role/elb" = 1
    }
-  provider "aws" {
-     region = "us-west-1"  # Specify the desired AWS region
+provider "aws" {
+   region = "us-west-1"  # Specify the desired AWS region
    }
  }
 
