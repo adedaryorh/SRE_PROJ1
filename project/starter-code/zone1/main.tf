eca0c18ef9c19d7de5f777e8a15202c2a14c0575
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-11-07 11:02:35 +0100

    Update main.tf

--- project/starter-code/zone1/main.tf
+++ project/starter-code/zone1/main.tf
@@ -25,25 +25,22 @@ locals {
  }
 
 module "vpc_west" {
-   source     = "./modules/vpc"
-   cidr_block = "10.100.0.0/16"
-   
-   account_owner = local.name
-   name          = "${local.name}-project"
-   azs           = ["us-west-1b","us-west-1c"]
-   private_subnet_tags = {
-   "kubernetes.io/role/internal-elb" = 1
-   }
-   public_subnet_tags = {
-   "kubernetes.io/role/elb" = 1
-   }
-provider "aws" {
-   aws = {
-      source = "hashicorp/aws"
-      version = ">= 3.0.0"
-      region = "us-west-1"
-    } 
-   }
+  source     = "./modules/vpc"
+  cidr_block = "10.100.0.0/16"
+
+  account_owner = local.name
+  name          = "${local.name}-project"
+  azs           = ["us-west-1b", "us-west-1c"]
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  providers = {
+    aws = aws.usw1
+  }
+}
 output "vpc_id" {
    value = module.vpc_west.vpc_id
  }
